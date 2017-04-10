64b26e0448821ef77bc7f2ab9e8e29bb5b569970
neverclear86 <neverclear86sab@gmail.com>
2017-04-10 23:06:48 +0900

    永遠に練りをしまつ( ˘ω˘)

---
+++ java/EternalSleep.java
@@ -0,0 +1,11 @@
+public class EternalSleep {
+  public static void main(String[] args) {
+    while (true) {
+      me.sleep();
+      System.out.println("( ˘ω˘)");
+      //永遠に練りをしまつ
+    }
+  }
+}
+
+// main内のみでインデント減らせばTwitterでも使えます
