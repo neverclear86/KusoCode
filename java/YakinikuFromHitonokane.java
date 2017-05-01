b42e793693141d69b526c546eb6e9ca1b6f6d45f
neverclear86 <neverclear86sab@gmail.com>
2017-05-01 20:00:18 +0900

    人の金で焼肉が食べたい更新

--- java/YakinikuFromHitonokane.java
+++ java/YakinikuFromHitonokane.java
@@ -1,1 +1,19 @@
-today.setDinner(@ID.getMoney().toYakiniku(Yakiniku.JOJO_EN));
+/**
+ * 人の金で焼肉が食べたい
+ */
+public final class Ogore {
+  public Ogore() {
+    Human me = Me.getInstance();
+    Human you = new Human("おまえ");
+    me.enter(YakinikuYa.JOJO_EN)
+      .orderAll(you.toATM().getMoney())
+      .parallelStream()
+      .forEach(me::eat);
+    you = null;
+    System.gc();
+  }
+
+  public static void main(String[] args) {
+    new Ogore();
+  }
+}
